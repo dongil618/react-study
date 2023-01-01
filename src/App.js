d4f4f66d288b58862127c8b27d6fe755e9b4b47c
dongil618 <rlaehddlf618@gmail.com>
2023-01-01 21:15:33 +0900

    chore : 컴포넌트
    - className, backgroundColor와 같이 CamelCase로 작성
    - css 코드는 중괄호({})로 감싸주기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '동일';
+  const naver = {
+    name: "네이버",
+    url : "https://www.naver.com"
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{
+        color: 'red',
+        backgroundColor: 'yellow'
+      }}
+      >
+        Welcome, {name} 내 나이는 {27+1}살 입니다.
+      </h1>
+      <a href={naver.url} target="_blank" rel="noopener noreferrer">{naver.name}</a>
     </div>
   );
 }
