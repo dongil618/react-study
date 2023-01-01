085503fd72e684fe02dc9f43397f7439b7c0f4fc
dongil618 <rlaehddlf618@gmail.com>
2023-01-01 23:09:45 +0900

    chore : 컴포넌트 만들기
    - https://devgrit.notion.site/4937c0bea9bb418fbd71ed41d2cd5555

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
 
 function App() {
-  const name = '동일';
-  const naver = {
-    name: "네이버",
-    url : "https://www.naver.com"
-  }
 
   return (
     <div className="App">
-      <h1 style={{
-        color: 'red',
-        backgroundColor: 'yellow'
-      }}
-      >
-        Welcome, {name} 내 나이는 {27+1}살 입니다.
-      </h1>
-      <a href={naver.url} target="_blank" rel="noopener noreferrer">{naver.name}</a>
+      <Hello/>
+      <Welcome/>
     </div>
   );
 }
