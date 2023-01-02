66c9fb1dbe8a21b2db1bd2320def60e6b141e520
dongil618 <rlaehddlf618@gmail.com>
2023-01-02 23:45:26 +0900

    chore : 이벤트 처리
    - https://www.notion.so/devgrit/391b7e71d5da49efb2039fb68954678c

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,17 +1,43 @@
-import World from "./World";
-import styles from "./Hello.module.css";
-
 const Hello = () => {
+    function showName() {
+        console.log("Mike");
+    }
+
+    function showAge(age) {
+        console.log(age);
+    }
+
+    function showText(e) {
+        console.log(e.target.value);
+    }
+
+    function showTxt(txt) {
+        console.log(txt);
+    }
+
     return(
         <div>
-            <h1 style={{
-                color : '#f00',
-                borderRight : '2px solid #000',
-                marginBottom : '30px',
-                opacity : 0.25
-            }}>Hello</h1>
-            <World/>
-            <div className={styles.box}>Hello</div>
+            <h1>Hello</h1>
+            <button onClick={showName}>Show name</button>
+            <button
+                onClick={() => {
+                    showAge(27);
+                }
+            }>Show age</button>
+            <input type="text" onChange={showText} />
+            {/* 위와 동일한 기능 */}
+            <input type="text" onChange={
+                (e) => {
+                    console.log(e.target.value);
+                }
+            } />
+            {/* 위와 동일한 기능 */}
+            <input type="text" onChange={
+                (e) => {
+                    const txt = e.target.value;
+                    showTxt(txt);
+                }
+            } />
         </div>
     )
 }
